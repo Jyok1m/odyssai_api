e6447c38598ebe344236dc19926821b09470cd82
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-07-15 17:18:35 +0200

    Added :
    - welcome.py
    - welcome html template
    - new clovis test

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
-from app.routes.hello import hello_bp
 from app.routes.parser import parser_bp
+from app.routes.welcome import welcome_bp
 
 def create_app():
     app = Flask(__name__)
 
     CORS(app)
     
-    app.register_blueprint(hello_bp, url_prefix="/")
-    app.register_blueprint(parser_bp, url_prefix="/parse")
+    # Enregistrer les blueprints
+    app.register_blueprint(welcome_bp)  # Route d'accueil à la racine
+    app.register_blueprint(parser_bp, url_prefix="/parser")
 
     return app
