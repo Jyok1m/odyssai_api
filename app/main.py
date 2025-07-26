4553395fd7f91354c7a686cd582d965e16bbc0bc
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-07-26 17:14:01 +0200

    Correction du nom de la collection dans le pipeline de requête et suppression de code commenté pour une meilleure clarté.

--- app/main.py
+++ app/main.py
@@ -11,12 +11,14 @@ if __name__ == "__main__":
         if pipeline_type == "query":
             result = query_pipeline.invoke(
                 {
-                    "collection_name": "test_collection",
+                    "collection_name": "worlds",
                     "n_results": 1,
                     "search_type": "mmr",
                     "credentials_path": "./secrets/google_tts.json",
-                    "language_code": "fr-FR",
-                    "voice_name": "fr-FR-Wavenet-D",
+                    # "language_code": "fr-FR",
+                    # "voice_name": "fr-FR-Wavenet-D",
+                    "language_code": "en-US",
+                    "voice_name": "en-US-Wavenet-D",
                 }
             )
             return result["speaker_file"]
@@ -30,8 +32,9 @@ if __name__ == "__main__":
             result = event_builder_pipeline.invoke({"world_name": world_name})
             return "Done"
 
+    # print(invoke_pipeline("query", "Elysia"))
     # result = invoke_pipeline("world", "Elysia")
     # result = invoke_pipeline("world", "Avros")
-    result = invoke_pipeline("lore", "Elysia")
+    # result = invoke_pipeline("lore", "Elysia")
     # result = invoke_pipeline("event", "Elysia")
-    print(result)
+    # print(result)
